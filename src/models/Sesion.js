0450a3bf5070e15ea6f7d44a72f7d31a61f8cf28
Edcko <edckoo1612@gmail.com>
2023-05-25 10:41:23 -0600

    Se modifico el modelo Sesion

--- src/models/Sesion.js
+++ src/models/Sesion.js
@@ -1,36 +1,45 @@
 import { db } from "../config/database.js";
 import { DataTypes, Model } from "sequelize";
+import { Empleado } from "./Empleado.js";
 
-class Sesion extends Model{}
+class Sesion extends Model {}
 
-Sesion.init({
+Sesion.init(
+  {
     id_sesion: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
-    numero_sesion:{
-    type: DataTypes.INTEGER,
-    allowNull: false,
+
+    id_empleado: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Empleado,
+        key: "id_empleado",
+      },
     },
-    descripcion:{
-    type: DataTypes.TEXT,
-    allowNull: false
+
+    numero_sesion: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
-    fecha: {
-    type: DataTypes.DATE,
-    allowNull: true
+
+    descripcion: {
+      type: DataTypes.TEXT,
     },
-    fecha_cancelacion:{
-    type: DataTypes.DATE,
-    allowNull: true
+
+    duracion: {
+      type: DataTypes.TIME,
     }
-},{
+  },
+  {
     sequelize: db.sequelize,
-    modelName: 'Sesion',
-    tableName: 'sesion',
+    modelName: "Sesion",
+    tableName: "sesion",
     timestamps: false,
-});
+  }
+);
 
 export { Sesion };
     
